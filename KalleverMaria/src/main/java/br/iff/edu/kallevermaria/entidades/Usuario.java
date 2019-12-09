a80d92d74f73ab82f72f117c927a098c1cf59603
MariaFernanda612 <mariafernandaoliveiradesouza4@gmail.com>
2019-12-09 16:59:20 -0200

    qualquercoisa

--- KalleverMaria/src/main/java/br/iff/edu/kallevermaria/entidades/Usuario.java
+++ KalleverMaria/src/main/java/br/iff/edu/kallevermaria/entidades/Usuario.java
@@ -1,4 +1,6 @@
-/*
+package br.iff.edu.kallevermaria.entidades;
+
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
